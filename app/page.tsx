33756fe86d7c3fe83ae159897a1bf7a6de435d3b
naomi-oleary <oleary.ng@gmail.com>
2026-09-03 21:43:01 -0600

    first official commit:

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { RiLeafLine } from 'react-icons/ri';
+import { BiCrown } from 'react-icons/bi';
+import { BsStarFill, BsStarHalf } from 'react-icons/bs';
+import { AiFillFileText, AiFillBulb, AiFillAudio } from 'react-icons/ai';
+import HomePage from './components/HomePage';
+import Image from 'next/image';
+
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen w-full">
+      <main>
+        <HomePage />
+      </main>
+    </div>
+  );
+}
