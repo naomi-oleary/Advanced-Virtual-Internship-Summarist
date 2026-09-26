a01f16d63bdb17e46784731d24c54ab2d7691802
naomi-oleary <oleary.ng@gmail.com>
2026-09-25 20:46:35 -0600

    fixed formatting, mapped over book tags

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -8,6 +8,7 @@ import { FaRegStar } from "react-icons/fa6";
 import { SlMicrophone } from "react-icons/sl";
 import { RiLightbulbLine } from "react-icons/ri";
 import { FiBookOpen } from "react-icons/fi";
+import { FaRegBookmark } from "react-icons/fa";
 
 
 export default async function BookPage({ params }: PageProps) {
@@ -21,7 +22,7 @@ export default async function BookPage({ params }: PageProps) {
         <NavBar />
         <div className="flex flex-col items-center text-blue-950">
             <img className="p-8 max-h-80" src={book.imageLink} />
-            <div>
+            <div className="items-start w-full p-8">
                 <h1 className="text-2xl font-extrabold">{book.title}</h1>
                 <p className="text-sm font-bold">{book.author}</p>
                 <h2 className="font-semilight">{book.subTitle}</h2>
@@ -49,15 +50,32 @@ export default async function BookPage({ params }: PageProps) {
                     </span>
                     <hr className="border-t border-gray-300" />
                 </div>
-                <div className="flex text-white p-4">
-                    <button className="flex bg-blue-950 p-4 rounded-sm">
+                <div className="flex text-white p-4 gap-8">
+                    <button className="flex bg-blue-950 p-4 gap-2 rounded-sm w-full max-w-40 items-center justify-center hover:bg-blue-950/75 transition">
                         <FiBookOpen />
                         <p>Read</p>
                     </button>
-                    <button className="flex bg-blue-950 p-4 rounded-sm">
+                    <button className="flex bg-blue-950 p-4 gap-2 rounded-sm w-full max-w-40 items-center justify-center  hover:bg-blue-950/75 transition">
                         <SlMicrophone />
                         <p>Listen</p>
                     </button>
+                </div>
+                <button className="text-blue-600 font-semibold flex gap-2 items-center">
+                    <FaRegBookmark />
+                    Add Title to My Library
+                </button>
+                <div>
+                    <h2 className="text-lg font-semibold py-6">What's it about?</h2>
+                    <span className="flex gap-6">
+                        {book.tags.map((tags, index) => (
+                            <p className="bg-green-800/10 max-w-80 text-sm font-semibold p-2 rounded-sm mb-6" key={index}>{tags}</p>
+                        ))}
+                    </span>
+                    <p className="text-sm">{book.bookDescription}</p>
+                </div>
+                <div>
+                    <h2 className="text-lg font-semibold py-6">About the Author</h2>
+                    <p className="text-sm">{book.authorDescription}</p>
                 </div>
             </div>
         </div>
